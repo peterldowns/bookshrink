dc2796c529718a628f69afd472ac586cc324f930
Peter Downs <peter.l.downs@gmail.com>
2011-08-29 18:21:25 -0400

    Learned how to access POST vars

--- btest.py
+++ btest.py
@@ -1,10 +1,27 @@
-from bottle import route, run, view
+from bottle import route, request, run, view, static_file
+
+#debugging
+from bottle import debug, run
+debug(True)
+
+class static_files():
+	""" Serve static files """
+	@route('/static/:path#.+#')
+	def server_static(path):
+	    return static_file(path, root='./static')
 
 class index():
+	# serves the main page
 	@route('/', 'GET')
-	@route('/:name#.+#', 'GET')
 	@view('index')
-	def get(name="Stranger"):
-		return dict(name=name)
+	def get():
+		return {}
+	@route('/', 'POST')
+	def post():
+		print "request:"
+		print request
+		print "forms:"
+		print request.forms
 
-run(host='localhost', port=8080)
+
+run(host='localhost', port=8080, reloader=True)
