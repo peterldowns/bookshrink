76569a1d9556d34dbeac790a5f4e3b1985368699
Peter Downs <peterldowns@gmail.com>
2014-07-13 13:54:24 -0400

    Use a set instead of a string for membership testing.

--- analysis.py
+++ analysis.py
@@ -19,14 +19,15 @@ class SentenceAnalyzer():
     # The ~100 most common words in the english language (which will be
     # ignored in the analysis), taken from
     # http://www.duboislc.org/EducationWatch/First100Words.html
-    commonstr = ("mr mrs ms dr the of and a to in is you that it he was for on "
-                 "are as with his they i at be this have from or one had by "
-                 "word but not what all were we when your can said there use "
-                 "an each which she do how their if will up other about out "
-                 "many then them these so some her would make like him into "
-                 "time has look two more write go see number no way could "
-                 "people my than first water been call who oil its now find "
-                 "long down day did get come made may part am let")
+    commonstr = set(
+            "mr mrs ms dr the of and a to in is you that it he was for on "
+            "are as with his they i at be this have from or one had by "
+            "word but not what all were we when your can said there use "
+            "an each which she do how their if will up other about out "
+            "many then them these so some her would make like him into "
+            "time has look two more write go see number no way could "
+            "people my than first water been call who oil its now find "
+            "long down day did get come made may part am let".split())
     # The darkest possible highlight color for a sentence when outputting
     # highlighted HTML.
     darkest_highlight_hex = '#FF0F0F'
